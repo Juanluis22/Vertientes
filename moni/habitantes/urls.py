e0f7f075a311668419f6f39df175edaed1de67a9
Vangauce <c.quilodran.ignacio@gmail.com>
2023-10-16 16:01:21 -0300

    Se añaden los template para eliminar usuario y Mi Perfil

--- moni/habitantes/urls.py
+++ moni/habitantes/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     
     #selector de comunidades para el admin
     path("lista_vertientes/<int:object_id>/", filtro, name='listvert'),
-    path("vertiente/<int:objecto_id>/", revision, name='verti'),
+    path("vertiente/<int:objecto_id>/<int:objecto_id_2>/", revision, name='verti'),
     path("detector/", detector, name='detect'),
     path('ph/<int:vertiente_id>/', views.grafico_ph, name='grafico_ph'),
     path('caudal/<int:vertiente_id>/', views.grafico_caudal, name='grafico_caudal'),
@@ -16,6 +16,7 @@ urlpatterns = [
     path('humedad/<int:vertiente_id>/', views.grafico_humedad, name='grafico_humedad'),
     path('temperatura/<int:vertiente_id>/', views.grafico_temperatura, name='grafico_temperatura'),
     path('turbiedad/<int:vertiente_id>/', views.grafico_turbiedad, name='grafico_turbiedad'),
+    path('miPerfil/<int:pk>/', ActualizarPerfil.as_view(), name='miPerfil'),
     
 
     #path("vertiente/<int:object_id>/", filtro, name='vert'),
--- moni/habitantes/views.py
+++ moni/habitantes/views.py
@@ -3,12 +3,15 @@ from django.views.generic import TemplateView, ListView,UpdateView,DetailView
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from nucleo.models import *
-from crud.forms import VertienteForm
+from user.models import User
+from crud.forms import VertienteForm, UpdateForm
 from nucleo.models import datos
+from django.urls import reverse, reverse_lazy
 from django.shortcuts import render
 from django.http import JsonResponse
 from nucleo.models import vertiente, comunidad
 import json
+
 
 # Create your views here.
 
@@ -43,36 +46,20 @@ def filtro(request, object_id):
     return render(request, 'vertientes/vertientes.html', {'objetos': objetos})
 
 
-@login_required
-def revision(request, objecto_id):
 
-    obj_id=get_object_or_404(vertiente,pk=objecto_id)
-    #print(obj_id)
-    id_foranea=obj_id.id
-    #print(id_foranea)
-    objetos2=datos.objects.filter(vertiente_id=id_foranea).first()
-   
-    
-    #print(objetos)
-
-    data={
-        'objetos':objetos2,
-        'vertiente':vertiente.objects.get(id=id_foranea)
-    }
-
-    return render(request, 'dashboard/dashboard.html', data)
 
 #fusionar ambos def revision
 @login_required
-def revision(request, objecto_id):
+def revision(request, objecto_id,objecto_id_2):
     obj_id = get_object_or_404(vertiente, pk=objecto_id)
     id_foranea = obj_id.id
     objetos2 = datos.objects.filter(vertiente_id=id_foranea).first()
     vertiente_info = vertiente.objects.get(id=id_foranea)
-
+    id_user=objecto_id_2
     comunidad_info = vertiente_info.comunidad
     
     data = {
+        'user_id':id_user,
         'objetos': objetos2,
         'vertiente': vertiente_info,
         'comunidad': comunidad_info
@@ -82,15 +69,18 @@ def revision(request, objecto_id):
 @login_required
 def detector(request):
     comu_id = request.user.comunidad_id
+    user_id=request.user.id
     #print(user_id)
-    
-
     objetos=vertiente.objects.filter(comunidad_id=comu_id)
 
+    context = {
+        'objetos': objetos,
+        'user_id': user_id,
+    }
+
+    return render(request, 'dashboard/vert.html',{'context': context})
 
 
-
-    return render(request, 'dashboard/vert.html',{'objetos': objetos})
 @login_required
 def grafico_ph(request, vertiente_id):
     datos_ph = datos.objects.filter(vertiente_id=vertiente_id)
@@ -169,3 +159,18 @@ def grafico_turbiedad(request, vertiente_id):
     }
     data_json = json.dumps(data)
     return render(request, 'dashboard/grafico_turbiedad.html', {'data_json': data_json})
+
+
+
+
+@method_decorator(login_required,name='dispatch')
+class ActualizarPerfil(UpdateView):
+    model = User
+    form_class = UpdateForm
+    template_name = 'perfil/miPerfil.html'
+    success_url = reverse_lazy('habi:detect')
+
+    
+
+    
+    
